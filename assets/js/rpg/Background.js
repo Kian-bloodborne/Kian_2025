d18f77bcd7509ae97861b007d2788b8d39963e56
Kian <kian1yong2@gmail.com>
2024-09-27 14:10:27 -0700

    Update Background.js

--- assets/js/rpg/Background.js
+++ assets/js/rpg/Background.js
@@ -32,21 +32,3 @@ export class Background {
 
 export default Background;
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Your Website</title>
-</head>
-<body>
-    <h1>Welcome to My Website</h1>
-
-    <audio autoplay loop>
-        <source src="path/to/your-music-file.mp3" type="audio/mpeg">
-        Your browser does not support the audio tag.
-    </audio>
-
-</body>
-</html>
-
