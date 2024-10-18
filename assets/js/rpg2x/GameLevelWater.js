4d0c8a7d093f6022e907eb217abbded379a86de2
Kian <kian1yong2@gmail.com>
2024-10-18 14:03:04 -0700

    Update GameLevelWater.js

--- assets/js/rpg2x/GameLevelWater.js
+++ assets/js/rpg2x/GameLevelWater.js
@@ -18,7 +18,7 @@ class GameLevelWater {
     let height = GameEnv.innerHeight;
 
     // Background data
-    const image_src_water = path + "/images/rpg/background.png";
+    const image_src_water = path + "/images/rpg/water.png";
     const image_data_water = {
         name: 'water',
         src: image_src_water,
@@ -27,15 +27,15 @@ class GameLevelWater {
 
     // Player 1 sprite data (turtle)
     const TURTLE_SCALE_FACTOR = 10;
-    const sprite_src_turtle = path + "/images/rpg/Knight.png";
+    const sprite_src_turtle = path + "/images/rpg/turtle.png";
     const sprite_data_turtle = {
         name: 'turtle',
         src: sprite_src_turtle,
         SCALE_FACTOR: TURTLE_SCALE_FACTOR,
         STEP_FACTOR: 1000,
-        ANIMATION_RATE: 500,
+        ANIMATION_RATE: 50,
         INIT_POSITION: { x: 0, y: height - (height/TURTLE_SCALE_FACTOR) }, 
-        pixels: {height: 280, width: 200},
+        pixels: {height: 280, width: 256},
         orientation: {rows: 4, columns: 3 },
         down: {row: 0, start: 0, columns: 3 },
         left: {row: 1, start: 0, columns: 3 },
